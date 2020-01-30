fde26a0653703afbd3690ccb285b7a492a8801e9
rosad92 <49042220+rosad92@users.noreply.github.com>
2020-01-29 20:57:12 -0500

    Implement Rigidbody

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
+
+        rb.AddForce();
     }
 
 }
