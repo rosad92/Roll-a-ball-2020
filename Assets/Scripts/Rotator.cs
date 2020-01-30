f66c076341e98497f7469c0bc939ca346a2d78ad
rosad92 <49042220+rosad92@users.noreply.github.com>
2020-01-30 11:05:54 -0500

    Fixed Typo in Previous Commit

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
+
 
 public class Rotator : MonoBehaviour
 {
@@ -9,6 +9,6 @@ public class Rotator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
     }
 }
