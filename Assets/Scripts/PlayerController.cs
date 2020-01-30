8878609782c9f8d9c60c20a6c8bbe28242fcb74b
rosad92 <49042220+rosad92@users.noreply.github.com>
2020-01-29 20:47:32 -0500

    PlayerController Script Setup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public class PlayerController : MonoBehaviour {
+
+    void FixedUpdate()
     {
-        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
+
+    
